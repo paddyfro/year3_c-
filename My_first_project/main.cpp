0f54e69fe3291074735cadc139f78f804821fc31
Patrick McD <paddyfro@gmail.com>
2018-09-17 20:36:42 +0100

    Add files via upload

---
+++ My_first_project/main.cpp
@@ -0,0 +1,32 @@
+//first c++ program!
+
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::string;
+using std::endl;
+using std::cin;
+
+/*
+void print_Ham() {
+	cout << "ham\n";
+}
+
+void get_Name() {
+	cout << "example" << endl;
+	string name;
+	cout << "Hello World!\n";
+	cout << "please enter your name:";
+	cin >> name;
+	cout << "hello " << name << endl;
+}
+*/
+int main() {
+	
+	//print_Ham();
+	//get_Name();
+	return 0;
+}
+
+
