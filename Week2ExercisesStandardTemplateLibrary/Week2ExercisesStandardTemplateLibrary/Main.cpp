bcf7ad2ec64375ca03bd9f03fae5e2eaae889d13
Patrick McD <paddyfro@gmail.com>
2018-09-27 10:57:56 +0100

    updated from in class work

--- Week2ExercisesStandardTemplateLibrary/Week2ExercisesStandardTemplateLibrary/Main.cpp
+++ Week2ExercisesStandardTemplateLibrary/Week2ExercisesStandardTemplateLibrary/Main.cpp
@@ -4,6 +4,7 @@
 #include <stdexcept>
 #include <vector>
 #include <list>
+#include <algorithm>
 
 
 using std::cout;
@@ -14,11 +15,21 @@ using std::ifstream;
 using std::ofstream;
 using std::vector;
 using std::list;
+using std::sort;
+using std::greater;
 
 void examp01();
 void vectorPrint(vector<int> vect);
 void examp02();
 void listPrint(list<int> numbers);
+void examp03();
+void examp04();
+void examp05();
+void examp06();
+double getMedian(vector<int> nums);
+void examp07();
+void print(const vector<int> &nums, const string prompt);
+
 
 int main() {
 	cout << "enter exercise number: " << endl;
@@ -31,6 +42,21 @@ int main() {
 		break;
 	case 2:
 		examp02();
+		break;
+	case 3:
+		examp03();
+		break;
+	case 4:
+		examp04();
+		break;
+	case 5:
+		examp05();
+		break;
+	case 6: 
+		examp06();
+		break;
+	case 7:
+		examp07();
 		break;
 	default:
 		cout << "dont no have exercise of that number (1-20)" << endl;
@@ -59,6 +85,7 @@ void examp01() {
 		cout << "error opening file" << endl;
 	}
 	*/
+	// vector<int> num(10); - set default size of vector
 	vector<int> numbers;
 	int num;
 	ifstream in("tester.txt");
@@ -74,20 +101,29 @@ void examp01() {
 	}
 	vectorPrint(numbers);
 
-	cout << "enter number you wish to search for: " << endl;
 	int userNum;
-	cin >> userNum;
-	bool found = false;
-	for (int i = 0; i < numbers.size(); i++)
-	{
-		if (numbers.at(i) == userNum) {
-			cout << "found at position: " << i << endl;
-			found = true;
+	do {
+		cout << "enter number you wish to search for: (-1 to exit)" << endl;
+
+		cin >> userNum;
+		bool found = false;
+		for (vector<int>::size_type i = 0; i < numbers.size(); i++)
+		{
+			if (numbers.at(i) == userNum) {
+				cout << "found at position: " << i << endl;
+				found = true;
+			}
+			/*
+			if (numbers[i] == userNum) {
+				cout << "found at position: " << i << endl;
+				found = true;
+			}
+			*/
 		}
-	}
-	if (!found) {
-		cout << "number not found!" << endl;
-	}
+		if (!found && userNum != -1) {
+			cout << "number not found!" << endl;
+		}
+	} while (userNum != -1);
 }
 
 void vectorPrint(vector<int> vect)
@@ -119,9 +155,10 @@ void examp02() {
 	cin >> userNum;
 	bool found = false;
 	int count = 0;
-	for (list<int>::iterator it = numbers.begin(); it != numbers.end(); ++it)
+	for (list<int>::iterator iter = numbers.begin(); iter != numbers.end(); ++iter)
 	{
-		if (*it == userNum) {
+		// *iter - dereference the iterator to return the value not the memory location reference
+		if (*iter == userNum) {
 			cout << "number found at position: " << count << endl;
 			found = true;
 		}
@@ -137,5 +174,173 @@ void listPrint(list<int> numbers) {
 	cout << "mylist contains:";
 	for (list<int>::iterator it = numbers.begin(); it != numbers.end(); ++it)
 		cout << ' ' << *it;
+	cout << '\n';
+}
+
+void examp03() {
+	vector<int> numbers;
+	int num;
+	ifstream in("tester.txt");
+	if (in) {
+		while (!in.eof())
+		{
+			in >> num;
+			numbers.push_back(num);
+		}
+	}
+	else {
+		cout << "error opening file" << endl;
+	}
+	vectorPrint(numbers);
+	//sort vector
+	sort(numbers.begin(), numbers.end());
+	vectorPrint(numbers);
+	/*
+	cout << "enter number you wish to search for: " << endl;
+	int userNum;
+	cin >> userNum;
+	bool found = false;
+	for (int i = 0; i < numbers.size(); i++)
+	{
+		if (numbers.at(i) == userNum) {
+			cout << "found at position: " << i << endl;
+			found = true;
+		}
+	}
+	if (!found) {
+		cout << "number not found!" << endl;
+	}
+	*/
+}
+
+
+void examp04() {
+	vector<int> numbers;
+	int num;
+	ifstream in("tester.txt");
+	if (in) {
+		while (!in.eof())
+		{
+			in >> num;
+			numbers.push_back(num);
+		}
+	}
+	else {
+		cout << "error opening file" << endl;
+	}
+	vectorPrint(numbers);
+	//sort vector
+	// https://stackoverflow.com/questions/9025084/sorting-a-vector-in-descending-order
+	sort(numbers.begin(), numbers.end(), greater<>());
+	vectorPrint(numbers);
+
+	cout << "enter number you wish to search for: " << endl;
+	int userNum;
+	cin >> userNum;
+	bool found = false;
+	for (int i = 0; i < numbers.size(); i++)
+	{
+		if (numbers.at(i) == userNum) {
+			cout << "found at position: " << i << endl;
+			found = true;
+		}
+	}
+	if (!found) {
+		cout << "number not found!" << endl;
+	}
+}
+
+/*
+bool greater(int a, int b)
+{
+	return a > b;
+}
+*/
+void examp05() {
+	vector<int> numbers;
+	int num;
+	ifstream in("tester.txt");
+	if (in) {
+		while (!in.eof())
+		{
+			in >> num;
+			numbers.push_back(num);
+		}
+	}
+	else {
+		cout << "error opening file" << endl;
+	}
+	
+
+	cout << "median is: " << getMedian(numbers) << endl;
+}
+
+double getMedian(vector<int> nums) {
+	int median = 0;
+	sort(nums.begin(), nums.end());
+	vectorPrint(nums);
+	vector<int>::size_type mid = nums.size() / 2;
+	if (nums.size() % 2 == 1) {
+		return nums.at(mid);
+	}
+	else {
+		median = (nums.at(mid - 1) + nums[mid]) / 2.0f;
+	}
+	return median;
+}
+
+void examp06() {
+	vector<int> numbers, numBelow, numAbove;
+	int num;
+	ifstream in("tester.txt");
+	if (in) {
+		while (!in.eof())
+		{
+			in >> num;
+			numbers.push_back(num);
+		}
+	}
+	else {
+		cout << "error opening file" << endl;
+	}
+	double median = getMedian(numbers);
+	sort(numbers.begin(), numbers.end());
+	for (vector<int>::size_type i = 0; i < numbers.size(); i++)
+	{
+		if (numbers[i] < median) {
+			numBelow.push_back(numbers[i]);
+		}
+		else
+		{
+			numAbove.push_back(numbers[i]);
+		}
+	}
+	cout << "Median: " << median << endl;
+	print(numAbove, "Above");
+	print(numBelow, "Below");
+}
+
+void examp07() {
+	vector<int> numbers;
+	int num;
+	ifstream in("tester.txt");
+	if (in) {
+		while (!in.eof())
+		{
+			in >> num;
+			numbers.push_back(num);
+		}
+	}
+	else {
+		cout << "error opening file" << endl;
+	}
+	print(numbers, "*********** My Vector *************");
+}
+
+void print(const vector<int> &nums, const string prompt)
+{
+	cout << prompt << endl;
+	for (unsigned i = 0; i < nums.size(); i++)
+		cout << ' ' << nums.at(i);
 	cout << '\n';
 }
